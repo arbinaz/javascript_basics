ae0814a4c1451b59e776d654f622b51e5708b6cc
arbinaz <arbinaz05@gmail.com>
2021-07-24 13:09:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// properties
+// 1. Java Script is dynamically typed
+// // 2. Popular technology
+
+
+// Two types of datatypes
+// 1. primitive
+// 2. Referential
+
+// primitive
+// 1.number
+// 2.string
+// 3.boolean
+// 4.null
+// 4.undefined
+// 5.symbol
+ var a=10;
+ var b='Joseph';
+ var c=true;
+ var d=null;
+ var e;
+
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ console.log(d);
+ console.log(e);
+
+ console.log(typeof a)
+ console.log(typeof b)
+ console.log(typeof c)
+ console.log(typeof d)
+ console.log(typeof e)
+// Hoisting
+ console.log(f);
+ var f=50;
